cb50a436fe923b172f40f3056b0d8ac515a7a437
hananraina <hananraina@gmail.com>
2023-02-08 22:59:14 +0530

    used reducer hook in cart context and user context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { createContext,useState } from "react";
+import { createContext,useReducer } from "react";
+import {createAction} from '../utils/reducer/reducer.utils';
 
 const addCartItem = (cartItems,productToAdd)=>{
 
@@ -16,7 +16,6 @@ const addCartItem = (cartItems,productToAdd)=>{
     return [...cartItems,{...productToAdd,quantity:1}];
 }
 
-
 const removeCartItem = (cartItems,cartItemToRemove)=>{
     //removed the find cart item to remove code, bec we already hv the item with quant.
 
@@ -31,49 +30,86 @@ const removeCartItem = (cartItems,cartItemToRemove)=>{
     )
 }
 
-
 const clearCartItem=(cartItems,cartItemToClear)=>cartItems.filter(cartItem=>cartItem.id != cartItemToClear.id);
 
 
 export const CartContext = createContext({
-    isCartOpen:false,
     setIsCartOpen:()=>{},
-    cartItems:[],
     addItemToCart:()=>{},
     removeItemToCart:()=>{},
     clearItemFromCart:()=>{},
+    isCartOpen:false,
+    cartItems:[],
     cartCount:0,
-    total:0
+    cartTotal:0
 })
 
+const CART_ACTION_TYPES={
+    SET_CART_ITEMS:'SET_CART_ITEMS',
+    TOGGLE_CART:'TOGGLE_CART'
+}
+
+const INITIAL_STATE={
+    isCartOpen:false,
+    cartItems:[],
+    cartCount:0,
+    total:0
+};
+
+const cartReducer = (state,action)=>{
+
+    const {type, payload} = action;
+
+    switch(type){
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            }
+        case CART_ACTION_TYPES.TOGGLE_CART:
+            return {
+                ...state,
+                isCartOpen:payload
+            }
+        default:
+            throw new Error(`Unhandled type of ${type} in cartReducer`)
+    }
+}
+
 export const CartProvider = ({children}) =>{
-    const [isCartOpen,setIsCartOpen]=useState(false);
-    const [cartItems,setCartItems]=useState([]);
-    const [cartCount,setCartCount]=useState(0);
-    const [cartTotal,setCartTotal]=useState(0);
-
-    useEffect(()=>{
-        const newCartCount = cartItems.reduce((total,cartItem)=>total + cartItem.quantity,0);
-        setCartCount(newCartCount)
-        },[cartItems])
-
-    useEffect(()=>{
-        const newCartTotal = cartItems.reduce((total,cartItem)=>total + (cartItem.quantity*cartItem.price),0);
-        setCartTotal(newCartTotal)
-        },[cartItems])
-
+    const [{isCartOpen,cartItems,cartCount,cartTotal},dispatch] = useReducer(cartReducer,INITIAL_STATE);
+       
+    const updateCartItemsReducer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce((total,cartItem)=>total + cartItem.quantity,0);
+        const newCartTotal = newCartItems.reduce((total,cartItem)=>total + (cartItem.quantity*cartItem.price),0);
+        
+        dispatch(
+            createAction(
+                CART_ACTION_TYPES.SET_CART_ITEMS,
+                {cartItems:newCartItems,
+                cartCount:newCartCount,
+                cartTotal:newCartTotal
+            }))
+    }    
+        
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems,productToAdd));
+      const newCartItems = addCartItem(cartItems,productToAdd);
+      updateCartItemsReducer(newCartItems);
     }
 
     const removeItemToCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems,cartItemToRemove));
+        const newCartItems = removeCartItem(cartItems,cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     }
 
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems,cartItemToClear));
+        const newCartItems =clearCartItem(cartItems,cartItemToClear);
+        updateCartItemsReducer(newCartItems);
     }
-
+    //acti
+    const setIsCartOpen = (bool) =>{
+        dispatch(createAction(CART_ACTION_TYPES.TOGGLE_CART,bool))
+    }
     const value={
         isCartOpen,
         setIsCartOpen,
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,17 +1,52 @@
-import { createContext,useState, useEffect } from "react";
-import { onAuthStateChangedListener,createUserDocumentFromAuth } from "../utils/firebase/firebase.utils.js";
+import { createContext,useEffect,useReducer } from "react";
+import { 
+    onAuthStateChangedListener,
+    createUserDocumentFromAuth }
+from "../utils/firebase/firebase.utils.js";
+import { createAction } from "../utils/reducer/reducer.utils.js";
+
 //actual value to access
 export const UserContext = createContext({
     currentUser:null,
     setCurrentUser: () => null,
 });
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER' : 'SET_CURRENT_USER'
+};
 
-//provider
+const userReducer = (state,action) =>{
+    const {type,payload} = action;
+
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+        return {
+            ...state,
+            currentUser : payload
+        }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+
+};
+const INITIAL_STATE = {
+    currentUser:null
+}
+
+
 export const UserProvider = ({children}) => {
-    const [currentUser,setCurrentUser]=useState(null);
+    
+    //useReducer(reducerFunc,state arg for reducerFunc)
+    //returns state, dispatch func which takes the action to userReducer function
+    const [{currentUser},dispatch]=useReducer(userReducer,INITIAL_STATE)
+    const setCurrentUser = (user)=>{
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user))
+    }
+
     const value = {currentUser,setCurrentUser};
+    
+    
     useEffect(()=>{
-        //callback fn
+
         const unsubscribe = onAuthStateChangedListener((user)=>{
         if(user){
             createUserDocumentFromAuth(user);
@@ -20,5 +55,6 @@ export const UserProvider = ({children}) => {
         })
     return unsubscribe;
     },[])
+
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
